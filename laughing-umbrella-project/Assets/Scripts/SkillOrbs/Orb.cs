2f14a448cf6e24d0f444a0ea0bc642ed114f408c
FavouredFool <t.wahrburg@gmail.com>
2021-08-28 15:05:05 +0200

    Orbtextur eingefügt und Orbs animiert

--- laughing-umbrella-project/Assets/Scripts/SkillOrbs/Orb.cs
+++ laughing-umbrella-project/Assets/Scripts/SkillOrbs/Orb.cs
@@ -7,8 +7,14 @@ public class Orb : MonoBehaviour {
     public SkillEnum.Skill skillEnum;
 
     // Höhere Zahl -> Langsamer
-    int CREATEANIMATIONSPEED = 25;
+    float CREATEANIMATIONSPEED = 25;
+    float MOVEMENTANIMATIONSPEED = 0.5f;
+
+    // Höhere Zahl -> geringere Amplitude
+    float MOVEMENTANIMATIONAPLITUDE = 10;
+        
     Vector3 maxScale;
+    Vector3 startPoint;
     #endregion
 
     
@@ -18,14 +24,19 @@ public class Orb : MonoBehaviour {
     public void Start()
     {
         maxScale = transform.localScale;
-
         transform.localScale = new Vector3(0, 0, 0);
+
+        startPoint = transform.position;
+
     }
     public void Update()
     {
         if (transform.localScale.x < maxScale.x)
         {
             transform.localScale += maxScale * 1/CREATEANIMATIONSPEED;
+        } else
+        {
+            transform.position = startPoint + new Vector3(0, Mathf.Sin(Time.time/MOVEMENTANIMATIONSPEED)/MOVEMENTANIMATIONAPLITUDE, 0);
         }
         
     }
