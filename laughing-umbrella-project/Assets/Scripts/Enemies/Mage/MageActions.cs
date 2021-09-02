3081b7b356211d4b8b55aadb59c67a6b26cd77c9
FavouredFool <t.wahrburg@gmail.com>
2021-09-02 17:20:43 +0200

    verbuggte Version des Magiers

--- laughing-umbrella-project/Assets/Scripts/Enemies/Mage/MageActions.cs
+++ laughing-umbrella-project/Assets/Scripts/Enemies/Mage/MageActions.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MageActions : MonoBehaviour {
 
@@ -8,12 +10,18 @@ public class MageActions : MonoBehaviour {
 
 
 	public float initialWaittime = 2f;
-	public float repeatActions = 6f;
-	public float fireTpWait = 3f;
+	public float waitAfterTp = 3f;
+	public float fireTpWait = 2f;
 
 	public float fireballSpeed = 2f;
 	public int fireballDamage = 1;
 
+	public float teleportDistance = 3;
+	public int raysCast = 8;
+
+	LayerMask teleportObstacles;
+
+	CapsuleCollider2D collider;
 	GameObject thrownFireball;
 
 	#endregion
@@ -22,25 +30,99 @@ public class MageActions : MonoBehaviour {
 
 	void Start()
     {
+		teleportObstacles = LayerMask.GetMask("Obstacle");
+		collider = GetComponent<CapsuleCollider2D>();
 
-		InvokeRepeating("doAction", initialWaittime, repeatActions);
+		//InvokeRepeating("doAction", initialWaittime, repeatActions);
+		StartCoroutine(DoAction());
     }
-	void doAction()
+	IEnumerator DoAction()
 	{
-		// 1. Fire fireball on target
+		// initial wait
+		yield return new WaitForSeconds(initialWaittime);
 
-		thrownFireball = Instantiate(fireball, gameObject.transform.position, Quaternion.identity);
+		while (true)
+        {
+			// 1. Fire fireball on target
 
-		Vector2 directionToPlayer = target.transform.position - gameObject.transform.position;
+			thrownFireball = Instantiate(fireball, gameObject.transform.position, Quaternion.identity);
 
-		thrownFireball.GetComponent<Fireball>().SetValues(directionToPlayer, fireballSpeed, fireballDamage);
-		
+			Vector2 directionToPlayer = target.transform.position - gameObject.transform.position;
 
+			thrownFireball.GetComponent<Fireball>().SetValues(directionToPlayer, fireballSpeed, fireballDamage);
 
-		// 2. wait
-		// 3. tp somewhere else
+			// 2. wait
+			yield return new WaitForSeconds(fireTpWait);
 
+			// 3. tp somewhere else
+			Vector3 position = findPosition();
+			gameObject.transform.position = position;
 
+			yield return new WaitForSeconds(waitAfterTp);
+		}
+
+	}
+
+	Vector3 findPosition()
+    {
+		int angle = (int)Random.Range(0, 360);
+		int angleFinal;
+		//Vector2[] possiblePositions = new Vector2[raysCast];
+		List<Vector2> possiblePositions = new List<Vector2>();
+		Vector2 direction;
+
+		for (int i=0; i<raysCast; i++)
+        {
+			// Define directions
+			angleFinal = ((angle + (360 / raysCast) * i) % 360) - 180;
+			
+			direction.x = Mathf.Sin(Mathf.Deg2Rad * angleFinal);
+			direction.y = Mathf.Cos(Mathf.Deg2Rad * angleFinal);
+			direction = direction.normalized;
+
+			if (validPos(direction, i))
+            {
+				possiblePositions.Add(gameObject.transform.position + new Vector3(direction.x * teleportDistance, direction.y * teleportDistance, 0));
+            }
+		}
+
+		// Check if pos was found
+		if (possiblePositions.Count <= 0)
+        {
+			return gameObject.transform.position;
+        }
+
+		// Find pos that's furthest away from Player
+		float maxDistanceToPlayer = float.NegativeInfinity;
+		Vector2 bestPos = gameObject.transform.position;
+
+		for (int i=0; i<possiblePositions.Count; i++)
+        {
+			float distance = Vector2.Distance(target.transform.position, possiblePositions[i]);
+			if (distance > maxDistanceToPlayer)
+            {
+				maxDistanceToPlayer = distance;
+				bestPos = possiblePositions[i];
+            }
+        }
+		return bestPos;
+    }
+
+	bool validPos(Vector2 direction, int i) {
+
+		// cast ray
+		if (!Physics2D.Raycast(gameObject.transform.position, direction, teleportDistance, teleportObstacles))
+        {
+			// keine Kollision gefunden
+			// Check ob Position in der Wand wäre 
+
+			if (!Physics2D.OverlapCapsule(gameObject.transform.position + new Vector3(direction.x * teleportDistance, direction.y * teleportDistance, 0), collider.size, collider.direction, 0f, teleportObstacles)) {
+				return true;
+			}
+
+			return false;
+        }
+		return false;
 	}
 
 	
