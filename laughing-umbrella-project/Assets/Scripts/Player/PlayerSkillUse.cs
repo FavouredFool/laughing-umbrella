b6764067a99784433e02f70e2f9b658d48edbb88
FavouredFool <t.wahrburg@gmail.com>
2021-09-09 21:37:21 +0200

    Dash bei Skill-Nutzung

--- laughing-umbrella-project/Assets/Scripts/Player/PlayerSkillUse.cs
+++ laughing-umbrella-project/Assets/Scripts/Player/PlayerSkillUse.cs
@@ -67,6 +67,11 @@ public class PlayerSkillUse : MonoBehaviour {
                 // Wenn man bei Linksklick direkt wieder auf Orb steht wird nähestehenster eingezogen:
                 checkForOrb();
 
+                if (playerActions.getDashCount() < 2)
+                {
+                    playerActions.setDashCount(playerActions.getDashCount() + 1);
+                }
+
             }
             else
             {
@@ -158,10 +163,7 @@ public class PlayerSkillUse : MonoBehaviour {
                 activeSkill = Instantiate(allSkills["SkillFire"], gameObject.transform);
                 break;
         }
-        if (playerActions.getDashCount() < 2)
-        {
-            playerActions.setDashCount(playerActions.getDashCount() + 1);
-        }
+        
     }
 
     public GameObject GetActiveSkill()
