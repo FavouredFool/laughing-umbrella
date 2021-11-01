f86aa081c8a71d5ffa772c4b72df7fc634160168
FortunesFavouredFool <tim.wahrburg@gmx.de>
2021-11-01 19:49:46 +0100

    Stunden aus dem Timer rausgenommen

--- laughing-umbrella-project/Assets/EndScript.cs
+++ laughing-umbrella-project/Assets/EndScript.cs
@@ -22,7 +22,7 @@ public class EndScript : MonoBehaviour
         startTime = Time.time;
         MainScript.totalTime = 222.212f;
         var ts = TimeSpan.FromSeconds(MainScript.totalTime);
-        timeText.text = "final time: \n" + string.Format("{0:00}:{1:00}:{2:00}:{3:000}",ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+        timeText.text = "final time: \n" + string.Format("{0:00}:{1:00}:{2:000}", ts.Minutes, ts.Seconds, ts.Milliseconds);
 
         clip = gameObject.GetComponent<VideoPlayer>();
         clip.waitForFirstFrame = true;
